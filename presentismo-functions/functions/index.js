4578e5e120b3fdd1f1fb25ccee8dd10c5ed04243
alecaceresprado <alecaceresprado@hotmail.com>
2022-09-19 15:53:21 +0100

    user table

--- presentismo-functions/functions/index.js
+++ presentismo-functions/functions/index.js
@@ -5,36 +5,53 @@ admin.initializeApp();
 
 const RFID = admin.firestore().collection('RFID');
 const AttendanceLog = admin.firestore().collection('AttendanceLog');
+const Users = admin.firestore().collection('Users');
+
+const getResource = (resource, where = []) => {
+  functions.logger.info("getResource: Start", { structuredData: true });
+  var ref = resource;
+  where.forEach(({ field, operator, value }) => {
+    ref = ref.where(field, operator, value);
+  });
+  functions.logger.info("getResource: finished parsing query", { structuredData: true });
+
+  return ref
+    .get()
+    .then((data) => {
+      let collection = [];
+      data.forEach(doc => {
+        collection.push(doc.data());
+      });
+      return collection;
+    })
+    .catch(e => functions.logger.error(e));
+}
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.https.onRequest((_, res) => {
   functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  res.send("Hello from Firebase!");
 });
 
 //
-exports.getAttendanceLog = functions.https.onRequest((request, response) => {
-  functions.logger.info("Fetching AttendanceLog", { structuredData: true });
-  AttendanceLog
-    .get()
-    .then((data) => {
-      let attendanceLog = [];
-      data.forEach(doc => {
-        attendanceLog.push(doc.data())
-      });
-      return response.json(attendanceLog)
-    })
-    .catch(e => functions.logger.error(e))
+exports.getAttendanceLog = functions.https.onRequest(async (_, res) => {
+  functions.logger.info("getAttendanceLog: Start", { structuredData: true });
+  const attendanceLog = await getResource(AttendanceLog);
+  return res.json(attendanceLog);
 });
 
-exports.postAttendanceLog = functions.https.onRequest(async (request, response) => {
-  functions.logger.info("postAttendanceLog: start", { structuredData: true });
+exports.postAttendanceLog = functions.https.onRequest(async (request, res) => {
+  functions.logger.info("postAttendanceLog: Start", { structuredData: true });
   const { rfid } = request.body;
+  const now = new Date();
   const newLog = {
     rfid,
-    date: admin.firestore.Timestamp.fromDate(new Date())
+    timeStamp: now,
+    dateHash: now.toISOString().split('T')[0],
+
   };
   functions.logger.info("postAttendanceLog: fetching owner", { structuredData: true });
   // get entry on rfid collection to get who owns it
@@ -48,10 +65,37 @@ exports.postAttendanceLog = functions.https.onRequest(async (request, response) 
   AttendanceLog
     .add(newLog)
     .then(() => {
-      response.json({ message: "success", owner: newLog.owner })
+      res.json({ message: "success", owner: newLog.owner })
     })
     .catch(e => {
       functions.logger.error(e);
-      response.status(500).json({ error: 'something went wrong' })
+      res.status(500).json({ error: 'something went wrong' })
     })
+});
+
+exports.getUsers = functions.https.onRequest(async (_, res) => {
+  functions.logger.info("getUsers: Start", { structuredData: true });
+  const users = await getResource(Users);
+  return res.json(users);
+});
+
+exports.getDailyUserTable = functions.https.onRequest(async (req, res) => {
+  functions.logger.info("getDailyUserTable: Start", { query: req.query }, { structuredData: true });
+  const [users, attendance] = await Promise.all(
+    [
+      getResource(Users),
+      getResource(AttendanceLog, [{ field: 'dateHash', operator: '==', value: `${req.query.date}` }])
+    ]
+  );
+
+  const userTable = users.map(({ userName }) => {
+    const log = attendance.find(({ owner }) => owner === userName);
+    return ({
+      userName,
+      isPresent: !!log,
+      time: log && new Date(log.timeStamp._seconds * 1000).toTimeString().split(' ')[0]
+    })
+  })
+
+  return res.json({ userTable });
 });
